3bf36bfdbc032c3e1122fd9fdc3520d408a18d3b
walterwj <277372664@qq.com>
2021-03-29 20:14:32 +0800

    Add function for tidb_kill.py

--- TiDB_Kill.py
+++ TiDB_Kill.py
@@ -3,6 +3,7 @@
 
 import argparse
 import pymysql
+import json
 
 # If Python is version 2.7, encoding problems can reload sys configuration
 try:
@@ -16,10 +17,16 @@ except:
 
 def main():
     args = parse_args()
-    _set_pwd = ""
+    ack = "no"
+    ct = 0
+    while ack == "no":
+        ct += 1
+        ack = mysql_execute()
+        print("Connection retries {}".format(ct))
+        if ct >= 100:
+            ack = "yes"
 
-
-def mysql_execute(*_sql):
+def mysql_execute():
     # Connect to MySQL and execute SQL commands
     args = parse_args()
     config = {
@@ -28,25 +35,41 @@ def mysql_execute(*_sql):
         "user": args.user,
         "password": args.password,
         "charset": 'utf8mb4',
-        "cursorclass": pymysql.cursors.DictCursor
+        # "cursorclass": pymysql.cursors.DictCursor
     }
 
     connection = pymysql.connect(**config)
     cursor = connection.cursor()
+    _sql = "select @@tidb_config;"
     try:
-        for sql in _sql:
-            cursor.execute(sql)
-            content = cursor.fetchall()
-            connection.commit()
+        cursor.execute(_sql)
+        content = cursor.fetchall()
+        connection.commit()
     except:
         print(
-            "SQL {} execution failed~ \nPlease check table or database exists or not!"
+            "SQL {} execution failed~"
             .format(_sql))
     finally:
-        cursor.close()
-        connection.close()
+        print(args.advertiseAddress, args.advertisePort)
+        config_c = json.loads(content[0][0])
+        if config_c["advertise-address"].strip() == args.advertiseAddress and str(config_c["port"]).strip() == args.advertisePort:
+            print("The TiDB IP is {}".format(config_c["advertise-address"]))
+            print("The TiDB IP Port is {}".format(config_c["port"]))
+            sql_kill = "kill tidb {}".format(args.sessionId)
+            _ct = str(input("Will execute: {}, y/n (default:yes)".format(sql_kill))) or "y"
+            if _ct == "y":
+                cursor.execute(sql_kill)
+            else:
+                print("SQL {} cancel execution".format(sql_kill))
+            ack = "yes"
+            cursor.close()
+            connection.close()
+        else:
+            cursor.close()
+            connection.close()
+            ack = "no"
 
-    return content
+    return ack
 
 
 def parse_args():
