48d6c4928b5311bf1e49fc82313bf8fdb0cd41ef
walterwj <277372664@qq.com>
2021-04-21 18:09:22 +0800

    update tidb_checksum

--- tidb_checksum.py
+++ tidb_checksum.py
@@ -11,16 +11,17 @@ def main():
     args = parse_args()
     dbname = args.database
     u_dbname, t_sql = "use {}".format(dbname), "show tables;"
-    t_table = mysql_execute(u_dbname, t_sql)
-    tso = mysql_execute1("select checkPoint from tidb_binlog.checkpoint")
+    t_table = mysql_execute("f", u_dbname, t_sql)
+    tso = mysql_execute("t", "select checkPoint from tidb_binlog.checkpoint")
     tso = json.loads(tso[0]["checkPoint"])
     ftso = tso["ts-map"]["primary-ts"]
     ttso = tso["ts-map"]["secondary-ts"]
     for tb in t_table:
         tb_name = tb["Tables_in_{}".format(dbname)]
         fkvs, fbytes = check_table(dbname, tb_name, ftso, "f")
-        tkvs, tbytes = check_table(dbname, tb_name, ttso,"t")
-        if fbytes == tbytes and fkvs == tkvs:
+        tkvs, tbytes = check_table(dbname, tb_name, ttso, "t")
+        # if fbytes == tbytes and fkvs == tkvs:
+        if fkvs == tkvs:
             print(
                 "Check successful, DB name is:{},Table name is:{}, bytes is {}, kvs is {}"
                 .format(dbname, tb_name, fbytes, fkvs))
@@ -35,55 +36,45 @@ def check_table(db_name, table_name, tso, mode):
     set_scan = "set tidb_checksum_table_concurrency = 200"
     set_time = "set tidb_snapshot='{}'".format(tso)
     if mode == "f":
-        checksum = mysql_execute("use {}".format(db_name), set_time, set_scan, check_sql)
+        checksum = mysql_execute("f", "use {}".format(db_name), set_time,
+                                 set_scan, check_sql)
     else:
-        checksum = mysql_execute1("use {}".format(db_name), set_time, set_scan,
-                                  check_sql)
+        checksum = mysql_execute("t", "use {}".format(db_name), set_time,
+                                 set_scan, check_sql)
     Total_kvs = checksum[0]["Total_kvs"]
     Total_bytes = checksum[0]["Total_bytes"]
 
     return Total_kvs, Total_bytes
 
 
-def mysql_execute1(*_sql):
+def mysql_execute(mode, *_sql):
     # connect mysql
     args = parse_args()
-    host = args.tmysql.split(':', 1)[0]
-    port = int(args.tmysql.split(':', 1)[1])
+    if mode == 'f':
+        host = args.fmysql.split(':', 1)[0]
+        port = int(args.fmysql.split(':', 1)[1])
 
-    config = {
-        "host": host,
-        "user": args.tuser,
-        "password": args.tpassword,
-        "port": port,
-        "charset": 'utf8mb4',
-        "cursorclass": pymysql.cursors.DictCursor
-    }
+        config = {
+            "host": host,
+            "user": args.fuser,
+            "password": args.fpassword,
+            "port": port,
+            "charset": 'utf8mb4',
+            "cursorclass": pymysql.cursors.DictCursor
+        }
+    else:
+        args = parse_args()
+        host = args.tmysql.split(':', 1)[0]
+        port = int(args.tmysql.split(':', 1)[1])
 
-    connection = pymysql.connect(**config)
-    cursor = connection.cursor()
-    for sql in _sql:
-        cursor.execute(sql)
-        content = cursor.fetchall()
-        connection.commit()
-
-    return content
-
-
-def mysql_execute(*_sql):
-    # connect mysql
-    args = parse_args()
-    host = args.fmysql.split(':', 1)[0]
-    port = int(args.fmysql.split(':', 1)[1])
-
-    config = {
-        "host": host,
-        "user": args.fuser,
-        "password": args.fpassword,
-        "port": port,
-        "charset": 'utf8mb4',
-        "cursorclass": pymysql.cursors.DictCursor
-    }
+        config = {
+            "host": host,
+            "user": args.tuser,
+            "password": args.tpassword,
+            "port": port,
+            "charset": 'utf8mb4',
+            "cursorclass": pymysql.cursors.DictCursor
+        }
 
     connection = pymysql.connect(**config)
     cursor = connection.cursor()
