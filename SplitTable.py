e9d99b002856e83ee9cdc46fe3bb8952411e1ebe
WalterWj <277372664@qq.com>
2022-10-26 17:48:43 +0800

    update splittable

--- SplitTable.py
+++ SplitTable.py
@@ -40,14 +40,101 @@ class SplitTable():
         cursor.close()
         connection.close()
 
-        print(content)
         return content
 
-    def tableSplit(self):
-        pass
+    def keys(self, db, table):
+        # sql
+        sql = '''
+        select
+            INDEX_ID,
+            INDEX_NAME,
+            tidb_decode_key(start_key) start,
+            tidb_decode_key(END_KEY) end
+        from
+            information_schema.tikv_region_status
+        where
+            DB_NAME = '{}'
+            and TABLE_NAME = '{}'
+        order by
+            INDEX_ID;
+        '''.format(db, table)
+        keys = self.mysqlconnect(sql)
 
-    def indexSplit(self):
-        pass
+        return keys
+
+    def parserkeys(self, ct):
+        try:
+            sct = eval(ct['start'])
+        except:
+            sct = {}
+        if sct.has_key('_tidb_rowid'):
+            sctv = []
+            sctv.append(sct['_tidb_rowid'])
+            sctv = tuple(sctv)
+        elif sct.has_key('handle'):
+            sctv = sct['handle'].values()
+            sctv = tuple(sctv[::-1])
+        elif sct.has_key('index_vals') and ct['INDEX_ID'] == sct['index_id']:
+            sctv = sct['index_vals'].values()
+            sctv = tuple(sctv[::-1])
+        else:
+            sctv = ('0',)
+
+        try:
+            ect = eval(ct['end'])
+        except:
+            ect = {}
+        if ect.has_key('_tidb_rowid'):
+            ectv = []
+            ectv.append(ect['_tidb_rowid'])
+            ectv = tuple(ectv)
+        elif ect.has_key('handle'):
+            ectv = ect['handle'].values()
+            ectv = tuple(ectv[::-1])
+        elif ect.has_key('index_vals') and ct['INDEX_ID'] == ect['index_id']:
+            ectv = ect['index_vals'].values()
+            ectv = tuple(ectv[::-1])
+        else:
+            ectv = ('0',)
+
+        if len(sctv) == 1:
+            sctv = str(sctv).replace(',', '')
+        if len(ectv) == 1:
+            ectv = str(ectv).replace(',', '')
+
+        allct = str(sctv) + ',' + str(ectv)
+        allct = allct.strip(',')
+        
+        type = ct['INDEX_NAME']
+
+        return type, allct
+    
+    def parserSql(self, db, tbl, ct):
+        for type, val in ct.items():
+            val = val.strip(',')
+            if type == 'None' or type == 'handle':
+                sql = 'split table `{}`.`{}` by {};'.format(db, tbl, val)
+            else:
+                sql = 'split table `{}`.`{}` index `{}` by {};'.format(db, tbl, type, val)
+            
+            
+            print(sql)
+        
+        return sql
+
+    def main(self):
+        args = self.parse_args()
+
+        db = args.tables.split('.')[0]
+        tb = args.tables.split('.')[1]
+        keys = self.keys(db, tb)
+        parsers = {}
+        for key in keys:
+            type, ct = self.parserkeys(key)
+            type = str(type)
+            parsers[type] = ct + ',' + parsers.get(type,'')
+        
+        self.parserSql(db, tb, parsers)
 
     def parse_args(self):
         # Incoming parameters
@@ -70,22 +157,11 @@ class SplitTable():
                             help="Database password, default: null",
                             default="tidb@123")
         parser.add_argument(
-            "-d",
-            dest="database",
-            help="Database name, for example: test,test1, default: None",
-            default=None)
-        parser.add_argument(
             "-t",
             dest="tables",
             help=
-            "Table name (database.table), for example: test.test,test.test2, default: None",
-            default=None)
-        parser.add_argument(
-            "-sh",
-            dest="stats_healthy",
-            help=
-            "Table stats healthy, If it is below the threshold, then analyze~",
-            default=100)
+            "Table name (database.table), for example: test.test default: None",
+            default='test.sbt3')
 
         args = parser.parse_args()
 
@@ -94,18 +170,4 @@ class SplitTable():
 
 if __name__ == "__main__":
     main = SplitTable()
-    _sql = '''
-    select
-	*,
-	start_key,
-	tidb_decode_key(start_key) b,
-	END_KEY ,
-	tidb_decode_key(END_KEY) b
-from
-	information_schema.tikv_region_status
-where
-	DB_NAME = 'test'
-	and TABLE_NAME = 'sbt2'
-	order by INDEX_ID ;
-    '''
-    main.mysqlconnect(_sql)
+    main.main()
